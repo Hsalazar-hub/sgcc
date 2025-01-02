7d3fcba82f77ff7f468789048efe85c5db72a957
Nahum Meneses <menesesnahum@gmail.com>
2025-01-02 18:52:30 -0400

    test organization_id

--- convex/http.ts
+++ convex/http.ts
@@ -5,6 +5,21 @@ import { httpAction } from "./_generated/server";
 
 const http = httpRouter();
 
+function findKeyRecursively(obj: any, keyToFind: string) {
+  if (!obj || typeof obj !== "object") return null;
+
+  for (const key of Object.keys(obj)) {
+    if (key === keyToFind) {
+      return obj[key];
+    }
+    const nestedResult: any = findKeyRecursively(obj[key], keyToFind);
+    if (nestedResult !== null) {
+      return nestedResult;
+    }
+  }
+  return null;
+}
+
 http.route({
   path: "/clerk",
   method: "POST",
@@ -46,16 +61,18 @@ http.route({
           });
           break;
         case "organizationMembership.created":
-          console.log("result: ", result);
-          
+          console.log("result completo: ", JSON.stringify(result, null, 2));
+
+          const orgId = findKeyRecursively(result.data, "organization_id");
+          console.log("Organization ID encontrado:", orgId);
+        
           await ctx.runMutation(internal.users.addOrgIdToUser, {
             tokenIdentifier: `https://${clerk}|${result.data.public_user_data.user_id}`,
-            orgId: result.data.organization.id,
+            orgId,
             role: result.data.role === "org:admin" ? "admin" : "member",
           });
 
           break;
-          
         case "organizationMembership.updated":
        
           await ctx.runMutation(internal.users.updateRoleInOrgForUser, {
