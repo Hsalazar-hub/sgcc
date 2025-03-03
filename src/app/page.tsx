e69f693477cf63829ebab19bab59fad32d86ca14
Hsalazar <hdsalazar20@gmail.com>
2025-03-03 18:04:46 -0400

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ export default function LandingPage() {
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <Link
-                href="/dashboard/files"
+                href="https://fun-aphid-82.accounts.dev/sign-in"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Empezar
