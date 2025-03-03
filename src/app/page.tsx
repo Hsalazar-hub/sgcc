c5ef8d68d388f10fc19f6cfe30a9f9ec4fac62eb
Hsalazar <hdsalazar20@gmail.com>
2025-03-03 18:16:26 -0400

    Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { SignedIn } from "@clerk/nextjs";
+import { SignedIn,SignedOut } from "@clerk/nextjs";
 export default function LandingPage() {
   return (
     <div className="bg-white">
@@ -35,12 +35,14 @@ export default function LandingPage() {
               eficiente!
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
+              <SignedOut>
               <Link
                 href="https://fun-aphid-82.accounts.dev/sign-in"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Empezar
               </Link>
+              </SignedOut>
               <SignedIn>
                 <Link
                   href="dahsboard/files"
