54711dcf0bb8120ebf8226942ab6556c8e4f82f3
Hsalazar <hdsalazar20@gmail.com>
2025-03-04 15:13:55 -0400

    F

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -1,11 +1,13 @@
-import Link from "next/link";
+
 
 export function Footer() {
   return (
     <div className="h-40 bg-gray-100 mt-12 flex items-center">
       <div className="container mx-auto flex flex-col sm:flex-row justify-between items-center text-center sm:text-left">
-        <div className="mb-4 sm:mb-0">SGCS</div>
-        <div>Todos los derechos reservados.</div>
+        <div className="mb-4 sm:mb-0 text-lg sm:text-xl">SGCS</div>
+        <div className="text-lg sm:text-l">Realizado por Héctor Salazar y Alejandro Girardi.</div>
+        <div className="text-lg sm:text-l">Todos los derechos reservados.</div>
+       
       </div>
     </div>
   );
