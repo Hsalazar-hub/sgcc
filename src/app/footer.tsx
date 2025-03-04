10c0b70b1f3cc2f494f16a51eab391b114f3ebed
Hsalazar <hdsalazar20@gmail.com>
2025-03-04 15:09:36 -0400

    R

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -2,15 +2,11 @@ import Link from "next/link";
 
 export function Footer() {
   return (
-    <footer className="bg-gray-100 mt-12 py-6">
+    <div className="h-40 bg-gray-100 mt-12 flex items-center">
       <div className="container mx-auto flex flex-col sm:flex-row justify-between items-center text-center sm:text-left">
-        <div className="mb-4 sm:mb-0">
-          <Link href="/">
-            <div className="text-lg font-bold text-gray-800">SGCS</div>
-          </Link>
-        </div>
-        <div className="text-gray-600">Todos los derechos reservados.</div>
+        <div className="mb-4 sm:mb-0">SGCS</div>
+        <div>Todos los derechos reservados.</div>
       </div>
-    </footer>
+    </div>
   );
 }
